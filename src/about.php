9c078823fb8c7b171d9660956abaf59268c7659d
Farkas <fcsabi789@gmail.com>
2022-05-26 10:02:45 +0200

    beadás

--- src/about.php
+++ src/about.php
@@ -1,3 +1,13 @@
+
+/*
+* File: index.php
+* Author: Farkas Csaba
+* Copyright: 2022,Farkas Csaba
+* Group: Szoft I/N
+* Date: 2022-05-26
+* Github: https://github.com/fcsabi/
+* Licenc: GNU GPL
+*/
 <?php
 
 echo file_get_contents('templates/head.html');
--- src/calc.php
+++ src/calc.php
@@ -1,3 +1,12 @@
+/*
+* File: calc.php
+* Author: Farkas Csaba
+* Copyright: 2022,Farkas Csaba
+* Group: Szoft I/N
+* Date: 2022-05-26
+* Github: https://github.com/fcsabi/
+* Licenc: GNU GPL
+*/
 <?php
 
 echo file_get_contents('templates/head.html');
@@ -5,6 +14,9 @@ echo file_get_contents('templates/head.html');
 
 
 $page= file_get_contents('templates/calc.html');
+
+
+
 $testtomeg=$_GET('testtomeg');
 $magassag=$_GET('magasság');
 $eletkor=$_GET('eletkor');
@@ -14,8 +26,9 @@ $area=calcArea($testtomeg,$magassag,$eletkor);
 function calcArea($testtomeg,$magassag,$eletkor){
 
     $kalorai=(13.397*$testtomeg)+(4.799*$magassag)-(5.667*$eletkor)+447.593;
-}
-$page=str_replace('{{RESULT}}')
+};
+
+$result=str_replace('{{RESULT}}',$result,$page);
 
 
 echo $page;
