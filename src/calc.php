1995ebeba1f7294a789729801b19c2e4fc40175a
Farkas <fcsabi789@gmail.com>
2022-05-26 09:14:05 +0200

    A szmitás nagyon nem müködik

--- src/calc.php
+++ src/calc.php
@@ -4,9 +4,21 @@ echo file_get_contents('templates/head.html');
 
 
 
-echo file_get_contents('templates/calc.html');
+$page= file_get_contents('templates/calc.html');
+$testtomeg=$_GET('testtomeg');
+$magassag=$_GET('magasság');
+$eletkor=$_GET('eletkor');
+
+$area=calcArea($testtomeg,$magassag,$eletkor);
+
+function calcArea($testtomeg,$magassag,$eletkor){
+
+    $kalorai=(13.397*$testtomeg)+(4.799*$magassag)-(5.667*$eletkor)+447.593;
+}
+$page=str_replace('{{RESULT}}')
 
 
+echo $page;
 
 
 echo file_get_contents('templates/foot.html');
